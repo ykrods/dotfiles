92c432b491702c1e94da0879c69a4a5404f119a0
ykrods <890082+ykrods@users.noreply.github.com>
2017-03-07 14:19:46 +0900

    add package list

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ for file in .tmux.conf .agignore
 do
     ln -is ~/dotfiles/$file ~/$file
 done
+
+brew install $(cat ~/dotfiles/brew.txt)
+
+virtualenv ~/.venv
+~/.venv/bin/pip install -r ~/dotfiles/requirements.txt
