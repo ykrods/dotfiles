f8d010fa8a72c35d89aa68270c23e02b970337a1
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-26 22:32:18 +0900

    macos setting

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# # Prerequisites
-#
-# - homebrew
-#
-# # [TIPS] brew on home
-# $ mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-
-if [ ! -f ~/.bash_profile ]; then
-   cp ~/dotfiles/.bash_profile ~/.bash_profile
-fi
-
-if ! grep '^\. ~/dotfiles/\.bashrc$' ~/.bashrc >/dev/null 2>&1; then
-    echo '. ~/dotfiles/.bashrc' >> ~/.bashrc
-fi
-
 for file in .tmux.conf .agignore
 do
     ln -is ~/dotfiles/$file ~/$file
